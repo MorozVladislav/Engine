bbb17aca3e83bcf71ac68bc21ba5e00bd6ab008d
Vladislav <mvs.vladislav@gmail.com>
2018-11-03 00:02:31 +0300

    graph.py self-review done

--- graph.py
+++ graph.py
@@ -6,63 +6,49 @@ from os.path import expanduser
 import networkx
 
 
+class Layouts(object):
+    BIPARTITE = 'BIPARTITE'
+    CIRCULAR = 'CIRCULAR'
+    KAMADA_KAWAI = 'KAMADA_KAWAI'
+    RANDOM = 'RANDOM'
+    RESCALE = 'RESCALE'
+    SHELL = 'SHELL'
+    SPRING = 'SPRING'
+    SPECTRAL = 'SPECTRAL'
+
+
 class Graph(object):
 
-    def __init__(self, path=None):
-        self.file = path
-        self.name = None
-        self.idx = None
-        self.points = None
-        self.lines = None
-        self.graph = None
+    LAYOUT_MAP = {
+        Layouts.BIPARTITE: networkx.bipartite_layout,
+        Layouts.CIRCULAR: networkx.circular_layout,
+        Layouts.KAMADA_KAWAI: networkx.kamada_kawai_layout,
+        Layouts.RANDOM: networkx.random_layout,
+        Layouts.RESCALE: networkx.rescale_layout,
+        Layouts.SHELL: networkx.shell_layout,
+        Layouts.SPRING: networkx.spring_layout,
+        Layouts.SPECTRAL: networkx.spectral_layout
+    }
 
-    def deserialize_data(self):
-        if self.file is None:
-            raise EmptyPath('Path is not specified')
-        with open(expanduser(self.file)) as input_file:
+    def __init__(self, path):
+        self.path = path
+        self.graph = networkx.Graph()
+        with open(expanduser(self.path)) as input_file:
             raw_data = load(input_file)
         self.name = raw_data['name']
         self.idx = raw_data['idx']
-        self.points = [(item['idx'], {'post_idx': item['post_idx']}) for item in raw_data['points']]
-        self.lines = [(item['points'][0], item['points'][1],
-                       {'idx': item['idx'], 'weight': item['length']}) for item in raw_data['lines']]
-        return self.points, self.lines
-
-    def build(self, layout='spring', **kwargs):
-        layouts = {
-            'bipartite': networkx.bipartite_layout,
-            'circular': networkx.circular_layout,
-            'kamada_kawai': networkx.kamada_kawai_layout,
-            'random': networkx.random_layout,
-            'rescale': networkx.rescale_layout,
-            'shell': networkx.shell_layout,
-            'spring': networkx.spring_layout,
-            'spectral': networkx.spectral_layout
-        }
-        self.deserialize_data()
-        self.graph = networkx.Graph()
+        self.points = []
+        for item in raw_data['points']:
+            self.points.append((item['idx'], {'post_idx': item['post_idx']}))
+        self.lines = []
+        for item in raw_data['lines']:
+            self.lines.append((item['points'][0], item['points'][1], {'idx': item['idx'], 'weight': item['length']}))
         self.graph.add_nodes_from(self.points)
         self.graph.add_edges_from(self.lines)
-        try:
-            coordinates = layouts[layout](self.graph, **kwargs)
-        except KeyError:
-            raise UnknownLayout('Unknown layout')
-        for point in self.points:
+
+    def get_coordinates(self, layout=Layouts.SPRING, **kwargs):
+        coordinates = self.LAYOUT_MAP[layout](self.graph, **kwargs)
+        points = self.points
+        for point in points:
             point[1]['x'], point[1]['y'] = coordinates[point[0]]
-        return self.points, self.lines
-
-
-class GraphException(Exception):
-    pass
-
-
-class UnknownLayout(GraphException):
-    pass
-
-
-class EmptyPath(GraphException):
-    pass
-
-
-if __name__ == '__main__':
-    pass
+        return points, self.lines
