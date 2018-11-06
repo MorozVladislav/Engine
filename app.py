933f38196a7edf63d3b29c62507656c4ac3c8131
stalkerboray <stalker.boray@gmail.com>
2018-11-06 15:57:23 +0300

    Add move point

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def prepare_coordinates(func):
                 x, y = int(point[1]['x'] * self.scale_x + self.x0), int(point[1]['y'] * self.scale_y + self.y0)
                 self.coordinates[point[0]] = (x, y)
         return func(self, *args, **kwargs)
+
     return wrapped
 
 
@@ -48,7 +49,8 @@ class Application(Frame, object):
         self.x0, self.y0, self.scale_x, self.scale_y = self.WIDTH / 2, self.HEIGHT / 2, None, None
         self.r = int(0.05 * min(self.x0, self.y0))
         self.font_size = self.r / 2
-        self.coordinates, self.weights, self.ids = {}, [], []
+        self.coordinates, self.weights, self.ids, self.objDict = {}, {}, [], {}
+        self.lineDict = {}
 
         self.menu = Menu(self)
         filemenu = Menu(self.menu)
@@ -64,6 +66,7 @@ class Application(Frame, object):
         self.frame = Frame(self)
         self.canvas = Canvas(self.frame, bg=self.BG, scrollregion=(0, 0, self.WIDTH, self.HEIGHT))
         self.canvas.config(width=self.WIDTH, height=self.HEIGHT)
+        self.canvas.bind('<B1-Motion>', self.movePoint)
         hbar = Scrollbar(self.frame, orient=HORIZONTAL)
         hbar.pack(side=BOTTOM, fill=X)
         hbar.config(command=self.canvas.xview)
@@ -98,7 +101,8 @@ class Application(Frame, object):
 
         self.canvas.delete('all')
         self.scale_x, self.scale_y = None, None
-        self.coordinates, self.weights, self.ids = {}, [], []
+        self.coordinates, self.weights, self.ids, self.objDict = {}, {}, [], {}
+        self.lineDict = {}
         self._graph = value
 
     def file_open(self):
@@ -135,8 +139,9 @@ class Application(Frame, object):
 
         for point in points:
             x, y = self.coordinates[point[0]]
-            self.canvas.create_oval(x - self.r, y - self.r, x + self.r, y + self.r, fill=self.POINT_COLOR)
-            self.canvas.create_text(x, y, text=point[0], font="{} {}".format(self.FONT, self.font_size))
+            point_id = self.canvas.create_oval(x - self.r, y - self.r, x + self.r, y + self.r, fill=self.POINT_COLOR)
+            text_id = self.canvas.create_text(x, y, text=point[0], font="{} {}".format(self.FONT, self.font_size))
+            self.objDict[point_id] = (text_id, point[0])
 
     @prepare_coordinates
     def create_lines(self, lines):
@@ -152,8 +157,9 @@ class Application(Frame, object):
         for line in lines:
             x_start, y_start = self.coordinates[line[0]]
             x_stop, y_stop = self.coordinates[line[1]]
-            self.canvas.create_line(x_start, y_start, x_stop, y_stop)
-            self.weights.append(((x_start + x_stop) / 2, (y_start + y_stop) / 2, line[2]['weight']))
+            line_id = self.canvas.create_line(x_start, y_start, x_stop, y_stop)
+            self.weights[line_id] = ((x_start + x_stop) / 2, (y_start + y_stop) / 2, line[2]['weight'])
+            self.lineDict[line_id] = (line[0], line[1])
         self.show_weights()
 
     def show_weights(self):
@@ -162,7 +168,7 @@ class Application(Frame, object):
         if len(self.weights) == 0:
             return
         if self.show_weight.get():
-            for weight in self.weights:
+            for weight in self.weights.values():
                 x, y, value = weight
                 r = int(self.r / 2) * len(str(value))
                 self.ids.append(self.canvas.create_oval(x - r, y - r, x + r, y + r, fill=self.BG, width=0))
@@ -170,6 +176,45 @@ class Application(Frame, object):
         else:
             for element_id in self.ids:
                 self.canvas.delete(element_id)
+
+        # move the point and edges
+
+    def movePoint(self, event):
+        # coord_id = self.canv.coords(event.x, event.y,)
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        obj_id = self.canvas.find_overlapping(x - 5, y - 5, x + 5, y + 5)
+        for obj in obj_id:
+            if obj in self.objDict.keys():
+                obj_number = self.objDict[obj][1]
+                self.canvas.coords(obj, event.x - self.r, event.y - self.r, event.x + self.r, event.y + self.r)
+                self.canvas.coords((self.objDict[obj][0]), event.x, event.y)
+                for key, values in self.lineDict.iteritems():
+                    new_point_x = None
+                    new_point_y = None
+                    # (11, 5, 113.0, 216.0, 61.0, 402.0) -> values
+                    if obj_number in values:
+                        if obj_number == values[0]:
+                            new_point_x, new_point_y = self.coordinates[values[1]]
+                            # new_point_y = values[5]
+                            # el self.lineDict[key]
+                            # self.lineDict[key] = (values[0], values[1], x, y, new_point_x, new_point_y)
+                            self.canvas.coords(key, x, y, new_point_x, new_point_y)
+
+                        elif obj_number == values[1]:
+                            new_point_x, new_point_y = self.coordinates[values[0]]
+                            # new_point_y = values[3]
+                            # del self.lineDict[key]
+                            # self.lineDict[key] = (values[0], values[1], new_point_x, new_point_y, x, y)
+                            self.canvas.coords(key, new_point_x, new_point_y, x, y)
+                        self.coordinates[obj_number] = (x, y)
+                        self.weights[key] = ((new_point_x + x) / 2, (new_point_y + y) / 2, self.weights[key][2])
+
+                        # self.canvas.coords(key, new_point_x, new_point_y, x, y)
+
+                        # self.lineDict[line_id] = (values[0], values[1], start_point_x,start_point_y,x ,y)
+
+                break
 
 
 if __name__ == '__main__':
